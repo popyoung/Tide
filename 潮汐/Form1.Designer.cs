fcf9afedde457db446012d5450ff1f62532de157
popyoung <popyoung@163.com>
2017-04-05 21:28:33 +0800

    一个带白噪音的番茄钟，抄袭自潮汐……

---
+++ 潮汐/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace 潮汐
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.设置ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.退出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.contextMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "潮汐";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.设置ToolStripMenuItem,
+            this.退出ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(101, 48);
+            // 
+            // 设置ToolStripMenuItem
+            // 
+            this.设置ToolStripMenuItem.Name = "设置ToolStripMenuItem";
+            this.设置ToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.设置ToolStripMenuItem.Text = "设置";
+            this.设置ToolStripMenuItem.Click += new System.EventHandler(this.设置ToolStripMenuItem_Click);
+            // 
+            // 退出ToolStripMenuItem
+            // 
+            this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
+            this.退出ToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.退出ToolStripMenuItem.Text = "退出";
+            this.退出ToolStripMenuItem.Click += new System.EventHandler(this.退出ToolStripMenuItem_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "无",
+            "钢琴",
+            "潮汐",
+            "雨天",
+            "森林",
+            "人声"});
+            this.comboBox1.Location = new System.Drawing.Point(103, 28);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(67, 20);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "背景音乐";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "工作时间";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "休息时间";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Location = new System.Drawing.Point(103, 71);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            120,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(67, 21);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            25,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Location = new System.Drawing.Point(103, 109);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(67, 21);
+            this.numericUpDown2.TabIndex = 5;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(203, 158);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "潮汐";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.contextMenuStrip1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ToolStripMenuItem 退出ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 设置ToolStripMenuItem;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+    }
+}
+
---
+++ 潮汐/Form1.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Media;
+using HugeHard.Json;
+using Newtonsoft.Json;
+
+namespace 潮汐
+{
+    public partial class Form1 : Form
+    {
+        class Config : JsonConfig<Form1>
+        {
+            public int 背景音乐
+            {
+                get { return Host.comboBox1.SelectedIndex; }
+                set { Host.comboBox1.SelectedIndex = Math.Min(value, Host.comboBox1.Items.Count - 1); }
+            }
+
+            public decimal 工作时间
+            {
+                get { return Host.numericUpDown1.Value; }
+                set { Host.numericUpDown1.Value = value; }
+            }
+            public decimal 休息时间
+            {
+                get { return Host.numericUpDown2.Value; }
+                set { Host.numericUpDown2.Value = value; }
+            }
+        }
+
+        [DllImport("kernel32.dll", EntryPoint = "SetProcessWorkingSetSize")]
+        public static extern int SetProcessWorkingSetSize(IntPtr process, int minSize, int maxSize);
+        public Form1()
+        {
+            InitializeComponent();
+            music_player.MediaEnded += Player_MediaEnded;
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void Player_MediaEnded(object sender, EventArgs e)
+        {
+            music_player.Position = new TimeSpan();
+        }
+
+        private void 退出ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            jsonHelper.Save("config.json", config);
+            Application.Exit();
+        }
+
+        private void 设置ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Normal;
+            //Show();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                switch (state)
+                {
+                    case States.未启动:
+                        StartWork();
+                        break;
+                    case States.工作:
+                    case States.休息:
+                        state = States.未启动;
+                        StopMusic();
+                        break;
+                }
+            }
+        }
+
+        List<string> music = new List<string>()
+        {
+            null,
+            "./resources/meditation.wma",
+            "./resources/ocean.wma",
+            "./resources/rain.wma",
+            "./resources/forest.wma",
+            "./resources/coffee.wma",
+        };
+
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left && e.Clicks == 1)
+            {
+                switch (state)
+                {
+                    case States.工作:
+                        if (countDown.TotalSeconds <= 0)
+                            StartBreak();
+                        break;
+                    case States.休息:
+                        if (countDown.TotalSeconds <= 0)
+                            StartWork();
+                        break;
+                }
+            }
+        }
+
+        private void StartWork()
+        {
+            countDown = new TimeSpan(0, (int)config.工作时间, 0);
+            state = States.工作;
+            PlayMusic(music[config.背景音乐]);
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void StartBreak()
+        {
+            countDown = new TimeSpan(0, (int)config.休息时间, 0);
+            state = States.休息;
+            StopMusic();
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        enum States
+        {
+            未启动,
+            工作,
+            休息,
+        }
+
+        States state;
+        TimeSpan countDown;
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            countDown = countDown.Subtract(new TimeSpan(0, 0, 1));
+            Thread.Sleep(1000);
+        }
+
+        int gc_count = 0;
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            ShowTip();
+            if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+            {
+                if (gc_count++ % 100 == 0)
+                {
+                    gc_count = 1;
+                    GC.Collect();
+                    SetProcessWorkingSetSize(Process.GetCurrentProcess().Handle, -1, -1);
+                }
+            }
+
+            if (state == States.未启动)
+                return;
+            if (countDown.TotalSeconds <= 0)
+            {
+                //switch (state)
+                //{
+                //    case States.工作:
+                //        StartBreak();
+                //        break;
+                //    case States.休息:
+                //        StartWork();
+                //        break;
+                //}
+                StopMusic();
+                player.Play();
+            }
+            else
+                backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void ShowTip()
+        {
+            StringBuilder s = new StringBuilder();
+            s.Append("潮汐: ");
+            s.AppendLine(state.ToString());
+            switch (state)
+            {
+                case States.未启动:
+                    s.AppendLine("双击开始工作!");
+                    break;
+                case States.工作:
+                    if (countDown.TotalSeconds <= 0)
+                        s.AppendLine("单击开始休息!");
+                    else
+                        s.AppendLine($"剩余时间: {countDown}");
+                    break;
+                case States.休息:
+                    if (countDown.TotalSeconds <= 0)
+                        s.AppendLine("单击开始工作!");
+                    else
+                        s.AppendLine($"剩余时间: {countDown}");
+                    break;
+            }
+            notifyIcon1.Text = s.ToString();
+        }
+
+        JsonHelper<Config, Form1> jsonHelper;
+        Config config;
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            jsonHelper = new JsonHelper<Config, Form1>(this);
+            config = jsonHelper.Load("config.json");
+            ShowTip();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                e.Cancel = true;
+                //Hide();
+                ShowInTaskbar = true;
+                WindowState = FormWindowState.Minimized;
+                ShowInTaskbar = false;
+            }
+        }
+
+        MediaPlayer music_player = new MediaPlayer();
+        SoundPlayer player = new SoundPlayer("./resources/清脆提示音.wav");
+        string lastMusic;
+        private void PlayMusic(string fileName)
+        {
+            if (fileName == null)
+                return;
+            if (fileName != lastMusic)
+            {
+                music_player.Close();
+                music_player.Open(new Uri(fileName, UriKind.Relative));
+                lastMusic = fileName;
+            }
+            music_player.Play();
+        }
+
+        private void StopMusic()
+        {
+            music_player.Stop();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
